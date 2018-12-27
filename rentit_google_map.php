28c2996fea0d6eacd87197385b7ef800a8b33e3f
myehrajat <31361589+myehrajat@users.noreply.github.com>
2018-12-27 20:48:21 +0330

    adding @ to prevent warning
    adding @ to prevent warning

--- rentit_google_map.php
+++ rentit_google_map.php
@@ -11,32 +11,33 @@ Version: 1.0
 Author URI: https://ma.tt/
 Text Domain: RentIt_Google_Map
 */
-include_once('customizer.php');
+include_once( 'customizer.php' );
 //line 566 of rentit/js/map_init.js there is a function which make map options in a variable names mapOptions2
-if(get_post_type( $_GET['post'] )=='product' or $_GET['post_type']== 'product' ){
-	add_action( 'admin_enqueue_scripts', 'RentIt_Google_Map_pass_mapOptions_vars',9999 );
+//use @ to prevent warning
+if ( ( get_post_type(@ $_GET[ 'post' ] ) == 'product'  or @ $_GET[ 'post_type' ] == 'product' ) ) {
+	add_action( 'admin_enqueue_scripts', 'RentIt_Google_Map_pass_mapOptions_vars', 9999 );
 }
-add_action( 'wp_enqueue_scripts', 'RentIt_Google_Map_pass_mapOptions_vars',9999 );
-	$mapOptionsVars = array(
-		//map_api
-		'api' =>get_theme_mod( 'map_api', 'AIzaSyCsbzuJDUEOoq-jS1HO-LUXW4qo0gW9FNs' ),
-		//map_max_zoom
-		'min_zoom' =>get_theme_mod( 'map_min_zoom', NULL ),
-		//map_max_zoom
-		'max_zoom' =>get_theme_mod( 'map_max_zoom', NULL ),
-		//map_zoom_control
-		'zoom_control' =>get_theme_mod( 'map_zoom_control', NULL ),
-		//map_zoom_scroll
-		'zoom_scroll' =>get_theme_mod( 'map_zoom_scroll', NULL ),
-		//map_map_type_control
-		'map_type_control' =>get_theme_mod( 'map_map_type_control', NULL ),
-		//map_language
-		'language' =>get_theme_mod( 'map_language', NULL ),
-	);
+add_action( 'wp_enqueue_scripts', 'RentIt_Google_Map_pass_mapOptions_vars', 9999 );
+$mapOptionsVars = array(
+	//map_api
+	'api' => get_theme_mod( 'map_api', 'AIzaSyCsbzuJDUEOoq-jS1HO-LUXW4qo0gW9FNs' ),
+	//map_max_zoom
+	'min_zoom' => get_theme_mod( 'map_min_zoom', NULL ),
+	//map_max_zoom
+	'max_zoom' => get_theme_mod( 'map_max_zoom', NULL ),
+	//map_zoom_control
+	'zoom_control' => get_theme_mod( 'map_zoom_control', NULL ),
+	//map_zoom_scroll
+	'zoom_scroll' => get_theme_mod( 'map_zoom_scroll', NULL ),
+	//map_map_type_control
+	'map_type_control' => get_theme_mod( 'map_map_type_control', NULL ),
+	//map_language
+	'language' => get_theme_mod( 'map_language', NULL ),
+);
 
 //pass variable
-function RentIt_Google_Map_pass_mapOptions_vars(){
+function RentIt_Google_Map_pass_mapOptions_vars() {
 	global $mapOptionsVars;
 	//pass momentVars object to js
-	wp_localize_script( 'renita_map_int', 'mapOptionsVars',$mapOptionsVars );
+	wp_localize_script( 'renita_map_int', 'mapOptionsVars', $mapOptionsVars );
 }
